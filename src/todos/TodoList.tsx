ffc958505dd48b025560962bbecc9d5025a69acb
Mauricio Robayo <rfmajo@gmail.com>
2020-10-30 12:54:32 -0500

    Add key and import Todo type

--- src/todos/TodoList.tsx
+++ src/todos/TodoList.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { Todo } from './TodoItem'
 
 type TodoListProps = {
-  todos: {
-    text: string
-  }[]
+  todos: Todo[]
 }
 
 const TodoList = ({ todos }: TodoListProps ) => {
   return (
     <>
-      {todos.map((todo) => <TodoItem todo={todo} />)}
+      {todos.map((todo) => <TodoItem key={todo.text} todo={todo} />)}
     </>
   )
 }
