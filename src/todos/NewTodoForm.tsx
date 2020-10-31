3d381e5a42dd0c83cc4156c554f20fe9475d49e7
Mauricio Robayo <rfmajo@gmail.com>
2020-10-31 07:21:38 -0500

    Connect NewTodoForm to the Redux store

--- src/todos/NewTodoForm.tsx
+++ src/todos/NewTodoForm.tsx
@@ -1,15 +1,43 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { Todo } from './TodoItem'
+import { createTodo } from '../actions'
 
-const NewTodoForm = () => {
+type NewTodoFromProps = {
+  todos: Todo[]
+  createTodo: (text: string) => void
+}
+
+const NewTodoForm = ({ todos, createTodo }: NewTodoFromProps) => {
   const [inputValue, setInputValue] = useState('');
+  const isDuplicateTodo = (text: string) => todos.some((todo) => todo.text === text)
+  const onCreateTodoHandler = () => {
+    if (!isDuplicateTodo(inputValue)) {
+      createTodo(inputValue)
+      setInputValue('')
+    }
+  }
 
   return (
     <>
       <label htmlFor='new-todo'>New Todo:</label>
       <input id='new-todo' type='text' placeholder='New Todo' value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
-      <button type="button">Create todo</button>
+      <button 
+        type="button"
+        onClick={onCreateTodoHandler}
+      >
+        Create todo
+      </button>
     </>
   )
 }
 
-export default NewTodoForm
+const mapStateToProps = (state: {todos: Todo[]}) => ({
+  todos: state.todos
+})
+
+const mapDispatchToProps = {
+  createTodo
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
