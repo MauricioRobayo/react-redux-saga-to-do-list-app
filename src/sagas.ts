27a9da96d856a2f6b53660758381f5c79dfbf1c7
Mauricio Robayo <rfmajo@gmail.com>
2020-11-01 15:32:06 -0500

    Use call to wrap fetch calls

--- src/sagas.ts
+++ src/sagas.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
-import { put, all, takeEvery } from 'redux-saga/effects';
+import { put, call, all, takeEvery } from 'redux-saga/effects';
 import { LOAD_TODOS, loadTodosSuccess, loadTodosFailure } from './actions'
 
 function* fetchTodos() {
   try {
-    const response = yield fetch("http://localhost:8080/todos-delay");
-    const todos = yield response.json();
+    const response = yield call(fetch, "http://localhost:8080/todos-delay");
+    const todos = yield call([response, 'json']);
     yield put(loadTodosSuccess(todos));
   } catch (e) {
     yield put(loadTodosFailure());
