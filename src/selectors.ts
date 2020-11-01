56382d9ab4c568403a0ee2709f890ee039b2e491
Mauricio Robayo <rfmajo@gmail.com>
2020-11-01 13:38:41 -0500

    Split into complete and incomplete TODOS

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,4 +1,15 @@
+import { createSelector } from 'reselect'
 import { AppState } from './store'
 
 export const getTodos = (state: AppState) => state.todos.data;
 export const getTodosLoading = (state: AppState) => state.todos.isLoading;
+
+export const getIncompleteTodos = createSelector(
+  getTodos,
+  (todos) => todos.filter((todo) => !todo.isCompleted)
+)
+
+export const getCompleteTodos = createSelector(
+  getTodos,
+  (todos) => todos.filter((todo) => todo.isCompleted)
+)
--- src/todos/TodoList.tsx
+++ src/todos/TodoList.tsx
@@ -1,51 +1,77 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { loadTodos, removeTodoRequest, markCompletedTodoRequest } from '../thunks'
+import {
+  loadTodos,
+  removeTodoRequest,
+  markCompletedTodoRequest,
+} from "../thunks";
 import TodoItem from "./TodoItem";
-import { Todo } from '../store'
+import { Todo } from "../store";
 import NewTodoForm from "./NewTodoForm";
-import { AppState } from '../store'
-import { getTodos, getTodosLoading } from '../selectors'
+import { AppState } from "../store";
+import {
+  getCompleteTodos,
+  getIncompleteTodos,
+  getTodosLoading,
+} from "../selectors";
 
 type TodoListProps = {
-  todos: Todo[];
+  completeTodos: Todo[];
+  incompleteTodos: Todo[];
   isLoading: boolean;
   removeTodoRequest: (id: string) => void;
   markCompletedTodoRequest: (id: string) => void;
   loadTodos: () => void;
 };
 
-const TodoList = ({ todos, isLoading, removeTodoRequest, markCompletedTodoRequest, loadTodos }: TodoListProps) => {
+const TodoList = ({
+  incompleteTodos,
+  completeTodos,
+  isLoading,
+  removeTodoRequest,
+  markCompletedTodoRequest,
+  loadTodos,
+}: TodoListProps) => {
   useEffect(() => {
-    loadTodos()
-  }, [loadTodos])
-  const loader = <div>Loading todos...</div>
+    loadTodos();
+  }, [loadTodos]);
+  const loader = <div>Loading todos...</div>;
   const content = (
     <>
       <NewTodoForm />
-      {todos.map((todo) => (
+      <h2>Incomplete TODOS</h2>
+      {incompleteTodos.map((todo) => (
+        <TodoItem
+        key={todo.text}
+        todo={todo}
+        removeTodo={removeTodoRequest}
+        markCompletedTodo={markCompletedTodoRequest}
+        />
+        ))}
+      <h2>Complete TODOS</h2>
+      {completeTodos.map((todo) => (
         <TodoItem
           key={todo.text}
           todo={todo}
           removeTodo={removeTodoRequest}
-          markCompletedTodo={markCompletedTodoRequest}
         />
       ))}
     </>
-  )
+  );
 
   return isLoading ? loader : content;
 };
 
 const mapStateToProps = (state: AppState) => ({
-  todos: getTodos(state),
-  isLoading: getTodosLoading(state)
+  completeTodos: getCompleteTodos(state),
+  incompleteTodos: getIncompleteTodos(state),
+  isLoading: getTodosLoading(state),
 });
 
 const mapDispatchToProps = {
   removeTodoRequest,
   markCompletedTodoRequest,
-  loadTodos
+  loadTodos,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
