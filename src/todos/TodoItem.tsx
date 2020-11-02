0e09fcbbd0a047955bf1e5e3825528aaa105159f
Mauricio Robayo <rfmajo@gmail.com>
2020-11-02 11:36:59 -0500

    Add TodoItems styles

--- src/todos/TodoItem.tsx
+++ src/todos/TodoItem.tsx
@@ -1,6 +1,20 @@
 import React from "react";
+import styled from 'styled-components/macro';
 
 import { Todo } from "../types";
+
+const TodoItemWrapper = styled.div`
+  background-color: white;
+  padding: 1rem;
+  display: flex;
+  border-radius: 4px;
+  margin: 1rem 0;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 type TodoProps = {
   todo: Todo;
@@ -10,17 +24,19 @@ type TodoProps = {
 
 const TodoItem = ({ todo, removeTodo, markCompletedTodo }: TodoProps) => {
   return (
-    <div>
+    <TodoItemWrapper>
       <h2>{todo.text}</h2>
-      <button type="button" onClick={() => removeTodo(todo.id)}>
-        Remove
-      </button>
-      {todo.isCompleted ? null : (
-        <button type="button" onClick={() => markCompletedTodo(todo.id)}>
-          Mark completed
+      <div className='buttons'>
+        <button type="button" onClick={() => removeTodo(todo.id)}>
+          Remove
         </button>
-      )}
-    </div>
+        {todo.isCompleted ? null : (
+          <button type="button" onClick={() => markCompletedTodo(todo.id)}>
+            Mark completed
+          </button>
+        )}
+      </div>
+    </TodoItemWrapper>
   );
 };
 
