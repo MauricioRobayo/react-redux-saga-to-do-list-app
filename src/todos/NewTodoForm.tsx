efceb3a86dd353c0ae4fccdb577e39de15c071ed
Mauricio Robayo <rfmajo@gmail.com>
2020-11-01 20:43:52 -0500

    Use html form for newTodo

--- src/todos/NewTodoForm.tsx
+++ src/todos/NewTodoForm.tsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AppState } from "../store";
 import { createTodo } from "../actions";
-import { getTodos } from '../selectors'
+import { getTodos } from "../selectors";
 
 const NewTodoForm = () => {
-  const todos = useSelector<AppState, AppState['todos']['data']>(getTodos)
+  const todos = useSelector<AppState, AppState["todos"]["data"]>(getTodos);
   const [inputValue, setInputValue] = useState("");
-  
+
   const dispatch = useDispatch();
 
   const isDuplicateTodo = (text: string) =>
     todos.some((todo) => todo.text === text);
-  const isEmptyTodo = (text: string) => 
-    text.trim().length === 0
-  
-    const onCreateTodoHandler = () => {
+  const isEmptyTodo = (text: string) => text.trim().length === 0;
+
+  const onCreateTodoHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
     if (!isEmptyTodo(inputValue) && !isDuplicateTodo(inputValue)) {
       dispatch(createTodo(inputValue));
       setInputValue("");
@@ -23,7 +23,7 @@ const NewTodoForm = () => {
   };
 
   return (
-    <>
+    <form>
       <label htmlFor="new-todo">New Todo:</label>
       <input
         id="new-todo"
@@ -32,10 +32,10 @@ const NewTodoForm = () => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       ></input>
-      <button type="button" onClick={onCreateTodoHandler}>
+      <button type="submit" onClick={onCreateTodoHandler}>
         Create todo
       </button>
-    </>
+    </form>
   );
 };
 
